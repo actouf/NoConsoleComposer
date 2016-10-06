e7bdeb0eaa7e0d658e84ca1cadfec553201cf7f0
actouf <actfly@hotmail.com>
2016-10-06 10:40:14 +0200

    Update index.php

--- composer/index.php
+++ composer/index.php
@@ -98,7 +98,7 @@ include 'password.php';
                 <h3>Commands:</h3>
                 <div class="form-inline">
                     <button id="self-update" onclick="del()" class="btn btn-success disabled">Update Composer</button><br /><br />
-                    <input type="text" id="path" style="width:300px;" class="form-control disabled" placeholder="absolute path to project directory"/>
+                    <input type="text" id="path" style="width:300px;" class="form-control disabled" placeholder="absolute path to project directory" value="<?php echo __DIR__ ?>"/>
                     <button id="install" onclick="call('install')" class="btn btn-success disabled">install</button>
                     <button id="update" onclick="call('update')" class="btn btn-success disabled">update</button>
                     <button id="update" onclick="call('dump-autoload')" class="btn btn-success disabled">dump-autoload</button>
